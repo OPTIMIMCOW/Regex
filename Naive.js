79fd8c83f3a25c9ded2a10268fbb2f0e5e4bdc67
Ken Allan <Ken.Allan@softwire.com>
2021-05-26 11:28:45 +0100

    test the cloning approach

--- Naive.js
+++ Naive.js
@@ -2,4 +2,31 @@
 
 var fs = require("fs"); // should allow us to use the system class to read other files. 
 
-fs.readFile()
+
+// try {  
+//     var data = fs.readFileSync('test.txt', 'utf8' );
+//     console.log(data.toString());    
+// } catch(e) {
+//     console.log('Error:', e.stack);
+// }
+
+
+
+// try {  
+//     var data2 = fs.readFile('test.txt', function(err, data) {
+//         res.write(data);
+//         return res.end();
+//       }); );
+
+//     console.log(data.toString());    
+// } catch(e) {
+//     console.log('Error:', e.stack);
+// }
+
+// fs.readFile('test.txt', 'utf8', function(err, data) {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+const text=fs.readFileSync('.\\test.txt', 'utf8')
+console.log('SUCCESSFULL RUN');
