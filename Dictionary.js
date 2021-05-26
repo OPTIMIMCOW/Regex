2e2b705fbb6d95d749d23660aeda81c7b713800d
Ken Allan <Ken.Allan@softwire.com>
2021-05-26 20:55:46 +0100

    cleaned up working dictionary code

--- Dictionary.js
+++ Dictionary.js
@@ -1,44 +1,34 @@
+
+//############### READ THE TEXT FILE INTO JAVASCRIPT ################//
 var fs = require("fs"); // should allow us to use the system class to read other files. 
 
 const text=fs.readFileSync('test.txt', 'utf8');
 
+//############### FIND THE EMAILS ADDRESSED IN TEXT ################//
 const regex = new RegExp(/@([A-z0-9])+.[A-z]+\S[A-z]+/gm);
-let arrMatched = text.match(regex);
+let arrFound = text.match(regex);
 
-var dictionary = {};
+//############### CREATE AND POPULATE OBJECT TO SUMMARISE EMAIL DOMAINS################//
+var dictionary = {}; // created an empty object and will add properties dynamically.
 
-for(let i = 0; i<arrMatched.length; i++){
-//console.log(arrMatched[i]);
-    //if domain is not in the list 
-    // create new key and give value as 1
-    //key exist, add value by 1
-    
-    if (dictionary.hasOwnProperty(arrMatched[i])){
-        dictionary[arrMatched[i]]++;
-        // let temp = dictionary.arrMatched[i];
-        // let tempName = arrMatched[i];
-        // dictionary.tempName = temp + 1;
+for(let i = 0; i<arrFound.length; i++){
+    if (dictionary.hasOwnProperty(arrFound[i])){ // note has own property is an inbuilt function so we do not need to write it. 
+        dictionary[arrFound[i]]++;
+
         
     } else {
-        dictionary[arrMatched[i]] = 1;
-    }
-
-    // for(let i = 0; i<dictionary.length; i++){
-    //     let tempMaxName;
-    //     tempMaxNumber;
-
-
-        
-    //     maxNumber = dictionary[i];
-
-    // }
+        dictionary[arrFound[i]] = 1;
+    }  
 
 }
 
+//############### SORT SUMMARY TO FIND MAX DOMAIN USER ################//
+
+//converting the dictionary object to an array
 let maxValue = 0;
 let maxKey;
 
-let tempArray = Object.entries(dictionary);
+let tempArray = Object.entries(dictionary); // Note entries is an inbuilt function of an object so we do not need to write it. 
 for (const [key, value] of Object.entries(dictionary)) {
     //console.log(`${key}: ${value}`);
     if(value>maxValue){
@@ -46,11 +36,10 @@ for (const [key, value] of Object.entries(dictionary)) {
         maxKey = key;
     }
 
-  }
-for(let i= 0; i<tempArray.length;)
-tempArray.sort();
+}
 
-//console.log(dictionary);
+//############### SHOW OUTPUTS ################//
+console.log(dictionary);
 //console.log(arrMatched.length);
 //console.log(Math.max(Object.values(dictionary)));
 console.log(maxValue + maxKey);
